66c6fe3ba80363a360052575a073528b9badca83
小羊 <tlylz99@live.cn>
2020-12-02 14:55:09 +0800

    Add modules projects.

---
+++ src/Host/DefaultContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SatelliteSite.IdentityModule.Entities;
+
+namespace SatelliteSite
+{
+    public class DefaultContext : IdentityDbContext<User, Role, int>
+    {
+        public DefaultContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ src/Host/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using SatelliteSite.IdentityModule.Entities;
+
+namespace SatelliteSite
+{
+    public class Program
+    {
+        public static IHost Current { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            Current = CreateHostBuilder(args).Build();
+            Current.AutoMigrate<DefaultContext>();
+            Current.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .MarkDomain<Program>()
+                .AddModule<IdentityModule.IdentityModule<User, Role, DefaultContext>>()
+                .AddDatabaseMssql<DefaultContext>("UserDbConnection")
+                .ConfigureSubstrateDefaults<DefaultContext>();
+    }
+}
