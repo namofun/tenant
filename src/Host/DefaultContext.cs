1656c8806b9c040fd5bdbe5ee0afdf4fdbe6237e
小羊 <tlylz99@live.cn>
2021-03-15 00:24:33 +0800

    Remove all dbcontext interfaces.

--- src/Host/DefaultContext.cs
+++ src/Host/DefaultContext.cs
@@ -1,44 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SatelliteSite.IdentityModule.Entities;
-using SatelliteSite.NewsModule;
-using SatelliteSite.NewsModule.Entities;
-using SatelliteSite.OjUpdateModule;
-using SatelliteSite.OjUpdateModule.Entities;
-using Tenant;
-using Tenant.Entities;
 
 namespace SatelliteSite
 {
-    public class DefaultContext :
-        IdentityDbContext<AspNetUser, Role, int>,
-        ITenantDbContext,
-        IGroupDbContext,
-        INewsDbContext,
-        IStudentDbContext,
-        IOJUpdateDbContext
+    public class DefaultContext : IdentityDbContext<AspNetUser, Role, int>
     {
         public DefaultContext(DbContextOptions options)
             : base(options)
         {
         }
-
-        public virtual DbSet<SolveRecord> SolveRecords { get; set; }
-
-        public virtual DbSet<Student> Students { get; set; }
-
-        public virtual DbSet<Class> Classes { get; set; }
-
-        public virtual DbSet<ClassStudent> ClassStudents { get; set; }
-
-        public virtual DbSet<News> News { get; set; }
-
-        public virtual DbSet<Affiliation> Affiliations { get; set; }
-
-        public virtual DbSet<Category> Categories { get; set; }
-
-        public virtual DbSet<GroupTeam> GroupTeams { get; set; }
-
-        public virtual DbSet<GroupUser> GroupUsers { get; set; }
     }
 }
