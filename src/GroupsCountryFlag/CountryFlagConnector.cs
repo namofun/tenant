b7d35edf5de6c48523e8c741cc84e64636b4f10c
小羊 <tlylz99@live.cn>
2021-07-04 21:29:04 +0800

    Add country flag connector.

---
+++ src/GroupsCountryFlag/CountryFlagConnector.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: AffiliateTo(
+    typeof(Tenant.Connector.CountryFlag.CountryFlagConnector),
+    typeof(SatelliteSite.GroupModule.GroupModule<>))]
+
+namespace Tenant.Connector.CountryFlag
+{
+    public class CountryFlagConnector : AbstractConnector
+    {
+        public override string Area => "Contest";
+
+        public override void RegisterServices(IServiceCollection services)
+        {
+            SatelliteSite.GroupModule.TenantDefaults.EnableCountryLogo = true;
+        }
+    }
+}
