b8adbf6d7624da0e6daa996f059191eb32290220
XiaoYang <tlylz99@live.cn>
2022-02-06 23:47:52 +0800

    Add back workflow related project.

---
+++ src/JobsCore/Works/Fallback.Unknown.cs
@@ -0,0 +1,16 @@
+﻿using Jobs.Entities;
+using Jobs.Services;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Jobs.Works
+{
+    public class FallbackUnknown : IJobExecutor
+    {
+        public Task<JobStatus> ExecuteAsync(string arguments, Job entry, ILogger logger)
+        {
+            logger.LogError("Unknown job type.");
+            return Task.FromResult(JobStatus.Unknown);
+        }
+    }
+}
