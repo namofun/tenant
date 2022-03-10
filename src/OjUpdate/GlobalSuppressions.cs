e566471dd8b6f4793ecec1854c66dea10f135c17
XiaoYang <tlylz99@live.cn>
2022-03-10 19:41:12 +0800

    Move News and OjUpdate to other project.

--- src/OjUpdate/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "CA1036:Override methods on comparable types", Justification = "No use.", Scope = "type", Target = "~T:SatelliteSite.OjUpdateModule.Models.OjAccount")]
-[assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "Needed.", Scope = "member", Target = "~P:SatelliteSite.OjUpdateModule.Models.CleanupModel.ToDelete")]
--- src/OjUpdate/Models/CleanupModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SatelliteSite.OjUpdateModule.Models
-{
-    public class CleanupModel
-    {
-        public int[] ToDelete { get; set; }
-
-        public int Category { get; set; }
-    }
-}
